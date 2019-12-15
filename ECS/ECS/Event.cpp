0f5da500e939f65660a75f763fe4fb44a8aac476
Smaugmuel <samuelasp96@gmail.com>
2019-12-15 15:59:00 +0100

    Removed old version of project

--- ECS/ECS/Event.cpp
+++
@@ -1,6 +0,0 @@
-#include "Event.h"
-
-namespace ECS
-{
-	BaseEvent::EventTypeID BaseEvent::s_event_id_counter = 0U;
-}
--- ECS/ECS/Utility.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-
-namespace Utils
-{
-	std::string prepend(const std::string& original, const size_t N, const char C);
-	std::string prependToLength(const std::string& original, const size_t N, const char C);
-}
