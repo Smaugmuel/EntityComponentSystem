576d9138e3238a6170fe950058cf9efcfdf9d835
Smaugmuel <samuelasp96@gmail.com>
2019-12-17 21:45:46 +0100

    Fixed some more comments

--- ECS_Project/Projects/Utilities/Utilities/HelperTemplates.hpp
+++ ECS_Project/Projects/Utilities/Utilities/HelperTemplates.hpp
@@ -6,23 +6,23 @@
 template<typename... T>
 struct TypeList {};
 
-// Checks if T is the same type as at least one of Ts
+// Checks if type T is the same type as at least one of Ts
 template<typename T, typename... Ts>
 using is_any_of = std::disjunction<std::is_same<T, Ts>...>;
 
-// Enables T if it is the same type as at least one of Ts
+// Evaluates to type T if it's the same type as at least one of Ts
 template<typename T, typename... Ts>
 using enable_if_any_of = std::enable_if<static_cast<bool>(is_any_of<T, Ts...>::value), T>;
 
-// Enables T if it is the same type as at least one of Ts, and extracts the type T
+// Evaluates to type T if it's the same type as at least one of Ts, and extracts the type
 template<typename T, typename... Ts>
 using enable_if_any_of_t = typename enable_if_any_of<T, Ts...>::type;
 
-// Enables R if T is the same type as at least one of Ts
+// Evaluates to type R if type T is the same type as at least one of Ts
 template<typename T, typename R, typename... Ts>
 using enable_R_if_any_of = std::enable_if<static_cast<bool>(is_any_of<T, Ts...>::value), R>;
 
-// Enables R if it is the same type as at least one of Ts, and extracts the type T
+// Evaluates to type R if type T is the same type as at least one of Ts, and extracts the type
 template<typename T, typename R, typename... Ts>
 using enable_R_if_any_of_t = typename enable_R_if_any_of<T, R, Ts...>::type;
 
