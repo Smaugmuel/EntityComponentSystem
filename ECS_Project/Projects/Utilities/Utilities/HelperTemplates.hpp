f4feb7ad148d8212b9718760ba7185b15b9fccc3
Smaugmuel <samuelasp96@gmail.com>
2020-01-07 19:53:41 +0100

    Added some comments

--- ECS_Project/Projects/Utilities/Utilities/HelperTemplates.hpp
+++ ECS_Project/Projects/Utilities/Utilities/HelperTemplates.hpp
@@ -2,9 +2,20 @@
 #include <type_traits>
 #include <tuple>
 
+/*
+	Default types
+*/
+
 // A representation of several data types without any stored data
-template<typename... T>
-struct TypeList {};
+template<typename... T> struct TypeList {};
+
+// Default type to allow TypeListing below
+template<typename... T> struct has_any_common {};
+
+
+/*
+	Bool expressions
+*/
 
 // Evaluates to true if type T is the same type as at least one of Ts
 template<typename T, typename... Ts>
@@ -17,13 +28,17 @@ struct is_any_of
 template<typename T, typename... Ts>
 inline constexpr bool is_any_of_v = is_any_of<T, Ts...>::value;
 
-// Evaluates to type T if it's the same type as at least one of Ts
-template<typename T, typename... Ts>
-using enable_if_any_of = std::enable_if<is_any_of_v<T, Ts...>, T>;
+// Evaluates to true if any of the types T are found in the types R
+template<typename... T, typename... R>
+struct has_any_common<TypeList<T...>, TypeList<R...>>
+{
+	static constexpr bool value = (is_any_of_v<T, R...> || ...);
+};
 
-// Evaluates to type T if it's the same type as at least one of Ts, and extracts the type
-template<typename T, typename... Ts>
-using enable_if_any_of_t = typename enable_if_any_of<T, Ts...>::type;
+
+/*
+	Type enablers
+*/
 
 // Evaluates to type R if type T is the same type as at least one of Ts
 template<typename T, typename R, typename... Ts>
@@ -33,17 +48,19 @@ using enable_R_if_any_of = std::enable_if<is_any_of_v<T, Ts...>, R>;
 template<typename T, typename R, typename... Ts>
 using enable_R_if_any_of_t = typename enable_R_if_any_of<T, R, Ts...>::type;
 
+// Evaluates to type T if it's the same type as at least one of Ts
+template<typename T, typename... Ts>
+using enable_if_any_of = enable_R_if_any_of<T, T, Ts...>;
+
+// Evaluates to type T if it's the same type as at least one of Ts, and extracts the type
+template<typename T, typename... Ts>
+using enable_if_any_of_t = typename enable_if_any_of<T, Ts...>::type;
+
+
+/*
+	Type mapping
+*/
+
 // Maps an int to a type
 template<int index, typename... Types>
 struct int_to_type { using T = typename std::tuple_element<index, std::tuple<Types...>>::type; };
-
-// Default type to allow for TypeListing below
-template<typename... T>
-struct has_any_common;
-
-// Evaluates to true if any of the types T are found in the types R
-template<typename... T, typename... R>
-struct has_any_common<TypeList<T...>, TypeList<R...>>
-{
-	static constexpr bool value = (is_any_of_v<T, R...> || ...);
-};
