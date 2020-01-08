6215518786430156bb62026f5c34e5b928f56294
Smaugmuel <samuelasp96@gmail.com>
2020-01-08 19:03:46 +0100

    Added namespace

--- ECS_Project/Projects/Utilities/Utilities/Utility.cpp
+++ ECS_Project/Projects/Utilities/Utilities/Utility.cpp
@@ -1,12 +1,15 @@
 #include "pch_Utilities.hpp"
 #include "Utility.hpp"
 
-std::string Utils::prepend(const std::string& original, const size_t N, const char C)
+namespace Utils
 {
-	return std::string(N, C) + original;
-}
+	std::string prepend(const std::string& original, const size_t N, const char C)
+	{
+		return std::string(N, C) + original;
+	}
 
-std::string Utils::prependToLength(const std::string& original, const size_t N, const char C)
-{
-	return prepend(original, N - original.size(), C);
+	std::string prependToLength(const std::string& original, const size_t N, const char C)
+	{
+		return prepend(original, N - original.size(), C);
+	}
 }
