83167eb81e191688350ce5b9acf17951ae92567a
Smaugmuel <samuelasp96@gmail.com>
2020-01-18 19:37:41 +0100

    Re-added function definitions into class definition of template classes

--- ECS_Project/Projects/ECS/ECS/Components/ComponentView.hpp
+++ ECS_Project/Projects/ECS/ECS/Components/ComponentView.hpp
@@ -45,175 +45,145 @@ namespace ECS
 		// Performs the passed function on each entity with all of the included components and none of the exlcuded ones
 		// The included components are sent as reference arguments to the function
 		template<typename Function>
-		void for_each_entity(Function f);
+		void for_each_entity(Function f)
+		{
+			if constexpr (sizeof...(IncludedTypes) == 1)
+			{
+				if constexpr (sizeof...(ExcludedTypes) == 0)
+				{
+					iterateSingleWithoutExcludes(f);
+				}
+				else
+				{
+					iterateSingleWithExcludes(f);
+				}
+			}
+			else
+			{
+				if constexpr (sizeof...(ExcludedTypes) == 0)
+				{
+					iterateMultiWithoutExcludes(f);
+				}
+				else
+				{
+					iterateMultiWithExcludes(f);
+				}
+			}
+		}
 
 		// Retrieves a pointer to a component of type T which is attached to an entity with the specified ID
 		// TODO: More work
 		template<typename CompType>
-		CompType* get(const EntityID entityID);		
+		CompType* get(const EntityID entityID)
+		{
+			static_assert(is_any_of_v<CompType, IncludedTypes...>, "CompType is not an included type");
+
+			if constexpr (is_singleton<CompType>::value)
+			{
+				return getPool<CompType>().components.get(0);
+			}
+			else
+			{
+				return getPool<CompType>().components.get(entityID);
+			}
+		}
 
 	public:
 		static constexpr Bitmask INCLUDED_MASK = calculateMask<IncludedTypes...>();
 		static constexpr Bitmask EXCLUDED_MASK = calculateMask<ExcludedTypes...>();
 
 	private:
-		// Retrieves a pointer to a component pool of type T
 		template<typename CompType>
-		ComponentPool<CompType>& getPool();
+		ComponentPool<CompType>& getPool()
+		{
+			constexpr bool is_included = is_any_of_v<CompType, IncludedTypes...>;
+			constexpr bool is_excluded = is_any_of_v<CompType, ExcludedTypes...>;
+			static_assert(static_cast<bool>(is_included || is_excluded), "CompType is not an included or excluded type");
+
+			if constexpr (is_included)
+			{
+				return *std::get<ComponentPool<CompType>*>(m_includedPools);
+			}
+			else // This else MUST remain, due to compile-time checks
+			{
+				return *std::get<ComponentPool<CompType>*>(m_excludedPools);
+			}
+		}
 
 		template<typename Func>
-		void iterateSingleWithoutExcludes(Func f);
+		void iterateSingleWithoutExcludes(Func f)
+		{
+			// Iterate components from the first included pool directly
+			auto& components = std::get<0>(m_includedPools)->components.getElements();
+
+			for (auto& comp : components)
+			{
+				f(comp);
+			}
+		}
 		template<typename Func>
-		void iterateSingleWithExcludes(Func f);
+		void iterateSingleWithExcludes(Func f)
+		{
+			// Iterate components from the first included pool directly and retrieve entity indices to check excluded components
+			auto& sparseSet = std::get<0>(m_includedPools)->components;
+			auto& components = sparseSet.getElements();
+			const auto& elemToIdx = sparseSet.getElemToIdx();
+			const size_t size = sparseSet.size();
+
+			for (size_t i = 0; i < size; i++)
+			{
+				const auto entityIdx = elemToIdx[i];
+				const bool hasAnyExcluded = (getPool<ExcludedTypes>().components.has(entityIdx) || ...);
+				if (!hasAnyExcluded)
+				{
+					f(components[i]);
+				}
+			}
+		}
 		template<typename Func>
-		void iterateMultiWithoutExcludes(Func f);
+		void iterateMultiWithoutExcludes(Func f)
+		{
+			// Iterate entity indices from the first included pool and check all included components
+			auto& sparseSet = std::get<0>(m_includedPools)->components;
+			const auto& elemToIdx = sparseSet.getElemToIdx();
+			const size_t size = sparseSet.size();
+
+			for (size_t i = 0; i < size; i++)
+			{
+				const auto entityIdx = elemToIdx[i];
+				const bool hasAllIncluded = (getPool<IncludedTypes>().components.has(entityIdx) || ...);
+				if (hasAllIncluded)
+				{
+					f(*getPool<IncludedTypes>().components.get(entityIdx)...);
+				}
+			}
+		}
 		template<typename Func>
-		void iterateMultiWithExcludes(Func f);
+		void iterateMultiWithExcludes(Func f)
+		{
+			// Iterate entity indices from the first included pool and check all included and excluded components
+			auto& sparseSet = std::get<0>(m_includedPools)->components;
+			const auto& elemToIdx = sparseSet.getElemToIdx();
+			const size_t size = sparseSet.size();
+
+			for (size_t i = 0; i < size; i++)
+			{
+				const auto entityIdx = elemToIdx[i];
+				const bool hasAllIncluded = (getPool<IncludedTypes>().components.has(entityIdx) && ...);
+				const bool hasAnyExcluded = (getPool<ExcludedTypes>().components.has(entityIdx) || ...);
+
+				const bool hasCorrectComponents = hasAllIncluded && !hasAnyExcluded;
+
+				if (hasCorrectComponents)
+				{
+					f(*getPool<IncludedTypes>().components.get(entityIdx)...);
+				}
+			}
+		}
 
 	private:
 		// Pointers to any number of component pools of different types
 		const std::tuple<ComponentPool<IncludedTypes>*...> m_includedPools;
 		const std::tuple<ComponentPool<ExcludedTypes>*...> m_excludedPools;
 	};
-	
-	template<typename ...IncludedTypes, typename ...ExcludedTypes>
-	template<typename CompType>
-	inline CompType * ComponentView<TypeList<IncludedTypes...>, TypeList<ExcludedTypes...>>::get(const EntityID entityID)
-	{
-		static_assert(is_any_of_v<CompType, IncludedTypes...>, "CompType is not an included type");
-
-		if constexpr (is_singleton<CompType>::value)
-		{
-			return getPool<CompType>().components.get(0);
-		}
-		else
-		{
-			return getPool<CompType>().components.get(entityID);
-		}
-	}
-	
-	template<typename ...IncludedTypes, typename ...ExcludedTypes>
-	template<typename CompType>
-	inline ComponentPool<CompType>& ComponentView<TypeList<IncludedTypes...>, TypeList<ExcludedTypes...>>::getPool()
-	{
-		constexpr bool is_included = is_any_of_v<CompType, IncludedTypes...>;
-		constexpr bool is_excluded = is_any_of_v<CompType, ExcludedTypes...>;
-		static_assert(static_cast<bool>(is_included || is_excluded), "CompType is not an included or excluded type");
-
-		if constexpr (is_included)
-		{
-			return *std::get<ComponentPool<CompType>*>(m_includedPools);
-		}
-		else // This else MUST remain, due to compile-time checks
-		{
-			return *std::get<ComponentPool<CompType>*>(m_excludedPools);
-		}
-	}
-
-
-	template<typename ...IncludedTypes, typename ...ExcludedTypes>
-	template<typename Function>
-	inline void ComponentView<TypeList<IncludedTypes...>, TypeList<ExcludedTypes...>>::for_each_entity(Function f)
-	{
-		if constexpr (sizeof...(IncludedTypes) == 1)
-		{
-			if constexpr (sizeof...(ExcludedTypes) == 0)
-			{
-				iterateSingleWithoutExcludes(f);
-			}
-			else
-			{
-				iterateSingleWithExcludes(f);
-			}
-		}
-		else
-		{
-			if constexpr (sizeof...(ExcludedTypes) == 0)
-			{
-				iterateMultiWithoutExcludes(f);
-			}
-			else
-			{
-				iterateMultiWithExcludes(f);
-			}
-		}
-	}
-
-	template<typename ...IncludedTypes, typename ...ExcludedTypes>
-	template<typename Func>
-	inline void ComponentView<TypeList<IncludedTypes...>, TypeList<ExcludedTypes...>>::iterateSingleWithoutExcludes(Func f)
-	{
-		// Iterate components from the first included pool directly
-		auto& components = std::get<0>(m_includedPools)->components.getElements();
-
-		for (auto& comp : components)
-		{
-			f(comp);
-		}
-	}
-
-	template<typename ...IncludedTypes, typename ...ExcludedTypes>
-	template<typename Func>
-	inline void ComponentView<TypeList<IncludedTypes...>, TypeList<ExcludedTypes...>>::iterateSingleWithExcludes(Func f)
-	{
-		// Iterate components from the first included pool directly and retrieve entity indices to check excluded components
-		auto& sparseSet = std::get<0>(m_includedPools)->components;
-		auto& components = sparseSet.getElements();
-		const auto& elemToIdx = sparseSet.getElemToIdx();
-		const size_t size = sparseSet.size();
-
-		for (size_t i = 0; i < size; i++)
-		{
-			const auto entityIdx = elemToIdx[i];
-			const bool hasAnyExcluded = (getPool<ExcludedTypes>().components.has(entityIdx) || ...);
-			if (!hasAnyExcluded)
-			{
-				f(components[i]);
-			}
-		}
-	}
-
-	template<typename ...IncludedTypes, typename ...ExcludedTypes>
-	template<typename Func>
-	inline void ComponentView<TypeList<IncludedTypes...>, TypeList<ExcludedTypes...>>::iterateMultiWithoutExcludes(Func f)
-	{
-		// Iterate entity indices from the first included pool and check all included components
-		auto& sparseSet = std::get<0>(m_includedPools)->components;
-		const auto& elemToIdx = sparseSet.getElemToIdx();
-		const size_t size = sparseSet.size();
-
-		for (size_t i = 0; i < size; i++)
-		{
-			const auto entityIdx = elemToIdx[i];
-			const bool hasAllIncluded = (getPool<IncludedTypes>().components.has(entityIdx) || ...);
-			if (hasAllIncluded)
-			{
-				f(*getPool<IncludedTypes>().components.get(entityIdx)...);
-			}
-		}
-	}
-
-	template<typename ...IncludedTypes, typename ...ExcludedTypes>
-	template<typename Func>
-	inline void ComponentView<TypeList<IncludedTypes...>, TypeList<ExcludedTypes...>>::iterateMultiWithExcludes(Func f)
-	{
-		// Iterate entity indices from the first included pool and check all included and excluded components
-		auto& sparseSet = std::get<0>(m_includedPools)->components;
-		const auto& elemToIdx = sparseSet.getElemToIdx();
-		const size_t size = sparseSet.size();
-
-		for (size_t i = 0; i < size; i++)
-		{
-			const auto entityIdx = elemToIdx[i];
-			const bool hasAllIncluded = (getPool<IncludedTypes>().components.has(entityIdx) && ...);
-			const bool hasAnyExcluded = (getPool<ExcludedTypes>().components.has(entityIdx) || ...);
-
-			const bool hasCorrectComponents = hasAllIncluded && !hasAnyExcluded;
-
-			if (hasCorrectComponents)
-			{
-				f(*getPool<IncludedTypes>().components.get(entityIdx)...);
-			}
-		}
-	}
 }
--- ECS_Project/Projects/ECS/ECS/ECSManager.hpp
+++ ECS_Project/Projects/ECS/ECS/ECSManager.hpp
@@ -32,49 +32,158 @@ namespace ECS
 		void clearEntities();
 
 		template<typename... IncludedTypes, typename... ExcludedTypes>
-		[[nodiscard]] ComponentView<TypeList<IncludedTypes...>, TypeList<ExcludedTypes...>> getView(TypeList<ExcludedTypes...> = {});
+		[[nodiscard]] ComponentView<TypeList<IncludedTypes...>, TypeList<ExcludedTypes...>> getView(TypeList<ExcludedTypes...> = {})
+		{
+			static_assert(sizeof...(IncludedTypes) > 0, "No included types");
+			static_assert(!has_any_common<TypeList<IncludedTypes...>, TypeList<ExcludedTypes...>>::value, "Included and excluded share a type");
+
+			return { getPool<IncludedTypes>()..., getPool<ExcludedTypes>()... };
+		}
 
 		template<typename CompType>
-		[[nodiscard]] bool hasComponent(const Entity& entity) const;
+		[[nodiscard]] bool hasComponent(const Entity& entity) const
+		{
+			static_assert(is_component<CompType>::value, "Not a component");
+			return hasComponent<CompType>(entity.ID);
+		}
 		template<typename CompType>
-		[[nodiscard]] bool hasComponent(EntityID entityID) const;
+		[[nodiscard]] bool hasComponent(EntityID entityID) const
+		{
+			static_assert(is_component<CompType>::value, "Not a component");
+			// 1 means maximum 32 bit shift. 1ULL means maximum 64 bit shift
+			return (m_componentMasks[entityID] & (1ULL << getID<CompType>()));
+		}
 
 		template<typename CompType, typename... TArgs>
-		[[maybe_unused]] CompType* attachComponent(const Entity& entity, const TArgs&... args);
+		[[maybe_unused]] CompType* attachComponent(const Entity& entity, const TArgs&... args)
+		{
+			static_assert(is_component<CompType>::value, "Not a component");
+			return attachComponent<CompType, TArgs...>(entity.ID, args...);
+		}
 		template<typename CompType, typename... TArgs>
-		[[maybe_unused]] CompType* attachComponent(EntityID entityID, const TArgs&... args);
+		[[maybe_unused]] CompType* attachComponent(EntityID entityID, const TArgs&... args)
+		{
+			static_assert(is_component<CompType>::value, "Not a component");
+
+			if (!isValid(entityID))
+			{
+				return nullptr;
+			}
+			if (!hasPool<CompType>())
+			{
+				createPool<CompType>();
+			}
+
+			ComponentPool<CompType>* pool = getPool<CompType>();
+
+			if constexpr (is_singleton<CompType>::value)
+			{
+				if (pool->components.size() == 0)
+				{
+					pool->components.add(0, args...);
+				}
+
+				if (!hasComponent<CompType>(entityID))
+				{
+					addToBitMask<CompType>(entityID);
+				}
+
+				return pool->components.get(0);
+			}
+			else
+			{
+				if (!hasComponent<CompType>(entityID))
+				{
+					pool->components.add(entityID, args...);
+					addToBitMask<CompType>(entityID);
+				}
+
+				return pool->components.get(entityID);
+			}
+		}
 
 		template<typename CompType>
-		void detachComponent(const Entity& entity);
+		void detachComponent(const Entity& entity)
+		{
+			static_assert(is_component<CompType>::value, "Not a component");
+
+			bool canBeDetached = isValid(entity.ID) && hasPool<CompType>() && hasComponent<CompType>(entity);
+			if (!canBeDetached)
+			{
+				return;
+			}
+
+			ComponentPool<CompType>* pool = getPool<CompType>();
+			pool->components.remove(entity.ID);
+			removeFromBitMask<CompType>(entity.ID);
+		}
 
 		template<typename CompType>
-		[[nodiscard]] size_t sizeOfPool() const;
+		[[nodiscard]] size_t sizeOfPool() const
+		{
+			return getPool<CompType>()->components.size();
+		}
 
 	private:
+		template<typename CompType>
+		static constexpr ComponentTypeID getID() noexcept
+		{
+			static_assert(is_component<CompType>::value, "Not a component");
+			return CompType::ID;
+		}
+
+		template<typename CompType>
+		bool hasPool() const
+		{
+			static_assert(is_component<CompType>::value, "Not a component");
+			static constexpr ComponentTypeID compTypeID = getID<CompType>();
+			return (compTypeID < m_componentPools.size() && m_componentPools[compTypeID]);
+		}
+
+		template<typename CompType>
+		void createPool()
+		{
+			static_assert(is_component<CompType>::value, "Not a component");
+			static constexpr size_t compTypeID = static_cast<const size_t>(getID<CompType>());
+			if (compTypeID >= m_componentPools.size())
+			{
+				m_componentPools.resize(compTypeID + 1, nullptr);
+			}
+			if (m_componentPools[compTypeID])
+			{
+				return;
+			}
+			m_componentPools[compTypeID] = new ComponentPool<CompType>;		// TODO: change allocation method
+		}
+
+		template<typename CompType>
+		ComponentPool<CompType>* getPool()
+		{
+			static_assert(is_component<CompType>::value, "Not a component");
+			static constexpr ComponentTypeID compTypeID = getID<CompType>();
+			return static_cast<ComponentPool<CompType>*>(m_componentPools[compTypeID]);
+		}
+
+		template<typename CompType>
+		void addToBitMask(EntityID entityID)
+		{
+			static_assert(is_component<CompType>::value, "Not a component");
+			m_componentMasks[entityID] |= (1ULL << getID<CompType>());
+		}
+
+		template<typename CompType>
+		void removeFromBitMask(EntityID entityID)
+		{
+			static_assert(is_component<CompType>::value, "Not a component");
+			m_componentMasks[entityID] &= ~(1ULL << getID<CompType>());
+		}
+
 		bool hasInvalidEntities() const noexcept;
 		EntityID getAndPopLastInvalidEntityID();
 		EntityID createNewEntity();
 		void resetAndValidateEntity(const EntityID entityID);
 		void resetComponentMask(const EntityID entityID);
 		void invalidateEntity(const EntityID entityID);
-
-		template<typename CompType>
-		static constexpr ComponentTypeID getID() noexcept;
-
-		template<typename CompType>
-		bool hasPool() const;
-
-		template<typename CompType>
-		void createPool();
-
-		template<typename CompType>
-		ComponentPool<CompType>* getPool();
-
-		template<typename CompType>
-		void addToBitMask(EntityID entityID);
-
-		template<typename CompType>
-		void removeFromBitMask(EntityID entityID);
 
 	private:
 		// Bitwise representation of which components each entity has
@@ -89,157 +198,4 @@ namespace ECS
 		// Previously created, but later invalidated, entity IDs
 		std::vector<EntityID> m_invalidEntityIDs;
 	};
-
-
-	/****************
-	** Definitions **
-	****************/
-
-	template<typename ...IncludedTypes, typename ...ExcludedTypes>
-	[[nodiscard]] inline ComponentView<TypeList<IncludedTypes...>, TypeList<ExcludedTypes...>> ECSManager::getView(TypeList<ExcludedTypes...>)
-	{
-		static_assert(sizeof...(IncludedTypes) > 0, "No included types");
-		static_assert(!has_any_common<TypeList<IncludedTypes...>, TypeList<ExcludedTypes...>>::value, "Included and excluded share a type");
-		
-		return { getPool<IncludedTypes>()..., getPool<ExcludedTypes>()... };
-	}
-
-	template<typename CompType>
-	[[nodiscard]] inline bool ECSManager::hasComponent(const Entity& entity) const
-	{
-		static_assert(is_component<CompType>::value, "Not a component");
-		return hasComponent<CompType>(entity.ID);
-	}
-
-	template<typename CompType>
-	inline bool ECSManager::hasComponent(EntityID entityID) const
-	{
-		static_assert(is_component<CompType>::value, "Not a component");
-		// 1 means maximum 32 bit shift. 1ULL means maximum 64 bit shift
-		return (m_componentMasks[entityID] & (1ULL << getID<CompType>()));
-	}
-
-	template<typename CompType, typename ...TArgs>
-	[[maybe_unused]] inline CompType* ECSManager::attachComponent(const Entity& entity, const TArgs& ...args)
-	{
-		static_assert(is_component<CompType>::value, "Not a component");
-		return attachComponent<CompType, TArgs...>(entity.ID, args...);
-	}
-	
-	template<typename CompType, typename ...TArgs>
-	[[maybe_unused]] inline CompType* ECSManager::attachComponent(EntityID entityID, const TArgs& ...args)
-	{
-		static_assert(is_component<CompType>::value, "Not a component");
-
-		if (!isValid(entityID))
-		{
-			return nullptr;
-		}
-		if (!hasPool<CompType>())
-		{
-			createPool<CompType>();
-		}
-
-		ComponentPool<CompType>* pool = getPool<CompType>();
-
-		if constexpr (is_singleton<CompType>::value)
-		{
-			if (pool->components.size() == 0)
-			{
-				pool->components.add(0, args...);
-			}
-
-			if (!hasComponent<CompType>(entityID))
-			{
-				addToBitMask<CompType>(entityID);
-			}
-
-			return pool->components.get(0);
-		}
-		else
-		{
-			if (!hasComponent<CompType>(entityID))
-			{
-				pool->components.add(entityID, args...);
-				addToBitMask<CompType>(entityID);
-			}
-
-			return pool->components.get(entityID);
-		}
-	}
-
-	template<typename CompType>
-	inline void ECSManager::detachComponent(const Entity & entity)
-	{
-		static_assert(is_component<CompType>::value, "Not a component");
-		
-		bool canBeDetached = isValid(entity.ID) && hasPool<CompType>() && hasComponent<CompType>(entity);
-		if (!canBeDetached)
-		{
-			return;
-		}
-
-		ComponentPool<CompType>* pool = getPool<CompType>();
-		pool->components.remove(entity.ID);
-		removeFromBitMask<CompType>(entity);
-	}
-
-	template<typename CompType>
-	inline size_t ECSManager::sizeOfPool() const
-	{
-		return getPool<CompType>()->components.size();
-	}
-
-	template<typename CompType>
-	inline constexpr ComponentTypeID ECSManager::getID() noexcept
-	{
-		static_assert(is_component<CompType>::value, "Not a component");
-		return CompType::ID;
-	}
-
-	template<typename CompType>
-	inline bool ECSManager::hasPool() const
-	{
-		static_assert(is_component<CompType>::value, "Not a component");
-		static constexpr ComponentTypeID compTypeID = getID<CompType>();
-		return (compTypeID < m_componentPools.size() && m_componentPools[compTypeID]);
-	}
-
-	template<typename CompType>
-	inline void ECSManager::createPool()
-	{
-		static_assert(is_component<CompType>::value, "Not a component");
-		static constexpr size_t compTypeID = static_cast<const size_t>(getID<CompType>());
-		if (compTypeID >= m_componentPools.size())
-		{
-			m_componentPools.resize(compTypeID + 1, nullptr);
-		}
-		if (m_componentPools[compTypeID])
-		{
-			return;
-		}
-		m_componentPools[compTypeID] = new ComponentPool<CompType>;
-	}
-
-	template<typename CompType>
-	inline ComponentPool<CompType>* ECSManager::getPool()
-	{
-		static_assert(is_component<CompType>::value, "Not a component");
-		static constexpr ComponentTypeID compTypeID = getID<CompType>();
-		return static_cast<ComponentPool<CompType>*>(m_componentPools[compTypeID]);
-	}
-
-	template<typename CompType>
-	inline void ECSManager::addToBitMask(EntityID entityID)
-	{
-		static_assert(is_component<CompType>::value, "Not a component");
-		m_componentMasks[entityID] |= (1ULL << getID<CompType>());
-	}
-
-	template<typename CompType>
-	inline void ECSManager::removeFromBitMask(EntityID entityID)
-	{
-		static_assert(is_component<CompType>::value, "Not a component");
-		m_componentMasks[entityID] &= ~(1ULL << getID<CompType>());
-	}
 }
